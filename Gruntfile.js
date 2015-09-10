2a17edb59ba97bd4b8678926a5d918171113bf88
Alvin Milton <alvin.milton@gmail.com>
2015-09-10 01:23:15 -0400

    updated package.json & grunt config

--- Gruntfile.js
+++ Gruntfile.js
@@ -10,82 +10,41 @@ module.exports = function (grunt) {
             '* Copyright (c) <%= grunt.template.today("yyyy") %> <%= pkg.author.name %>;' +
             ' Licensed <%= props.license %> */\n',
         // Task configuration
-        concat: {
-            options: {
-                banner: '<%= banner %>',
-                stripBanners: true
-            },
+        sass: {
             dist: {
-                src: ['lib/riot-spa.js'],
-                dest: 'dist/riot-spa.js'
+                files: [{
+                    expand: true,
+                    cwd: 'css',
+                    src: ['*.scss'],
+                    dest: 'css',
+                    ext: '.css'
+                }]
             }
         },
-        uglify: {
-            options: {
-                banner: '<%= banner %>'
-            },
-            dist: {
-                src: '<%= concat.dist.dest %>',
-                dest: 'dist/riot-spa.min.js'
-            }
-        },
-        jshint: {
-            options: {
-                node: true,
-                curly: true,
-                eqeqeq: true,
-                immed: true,
-                latedef: true,
-                newcap: true,
-                noarg: true,
-                sub: true,
-                undef: true,
-                unused: true,
-                eqnull: true,
-                browser: true,
-                globals: { jQuery: true },
-                boss: true
-            },
-            gruntfile: {
-                src: 'gruntfile.js'
-            },
-            lib_test: {
-                src: ['lib/**/*.js', 'test/**/*.js']
-            }
-        },
-        qunit: {
-            files: ['test/**/*.html']
-        },
-        watch: {
-            gruntfile: {
-                files: '<%= jshint.gruntfile.src %>',
-                tasks: ['jshint:gruntfile']
-            },
-            lib_test: {
-                files: '<%= jshint.lib_test.src %>',
-                tasks: ['jshint:lib_test', 'qunit']
-            }
-        },
-        serve: {
-            path: '/',
-            options: {
-                port: 9000
+        'http-server': {
+            root: 'index.html',
+            port: 8282,
+            host: "127.0.0.1",
+            cache: 10,
+            showDir : true,
+            autoIndex: true,
+            ext: "html",
+            runInBackground: true|false,
+            cors: true,
+            // logFn: requestLogger,
+            https: {
+                cert: "<file>",
+                key:  "<file>"
             }
         }
     });
 
     
     // These plugins provide necessary tasks
-    grunt.loadNpmTasks('grunt-contrib-concat');
-    grunt.loadNpmTasks('grunt-contrib-uglify');
-    grunt.loadNpmTasks('grunt-contrib-qunit');
-    grunt.loadNpmTasks('grunt-contrib-jshint');
-    grunt.loadNpmTasks('grunt-contrib-watch');
-    grunt.loadNpmTasks('grunt-serve');
-
-
+    grunt.loadNpmTasks('grunt-contrib-sass');
+    grunt.loadNpmTasks('grunt-http-server');
 
     // Default task
-    grunt.registerTask('default', ['jshint', 'qunit', 'concat', 'uglify']);
+    grunt.registerTask('default', ['sass', 'http-server']);
 };
 
