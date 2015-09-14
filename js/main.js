1a57acd0df94d3b9b41d9a8e00ce899921669a81
Alvin Milton <vinnypesos@gmail.com>
2015-09-14 12:10:38 -0400

    ignore js files

--- js/main.js
+++ js/main.js
@@ -47,6 +47,7 @@ riot.tag('page-header', '<header><h2>{this.componentTitle}</h2></header>', funct
 riot.tag('site-meta', '<meta http-equiv="X-UA-Compatible" content="IE=edge"><title>testing Riot JS</title><link href="https://maxcdn.bootstrapcdn.com/bootswatch/3.3.5/cosmo/bootstrap.min.css" rel="stylesheet" integrity="sha256-IF1P9CSIVOaY4nBb5jATvBGnxMn/4dB9JNTLqdxKN9w= sha512-UsfHxnPESse3RgYeaoQ7X2yXYSY5f6sB6UT48+F2GhNLqjbPhtwV2WCUQ3eQxeghkbl9PioaTOHNA+T0wNki2w==" crossorigin="anonymous"><link rel="stylesheet" href="css/style.css">', function(opts) {
 
 });
+
 riot.tag('top-menu', '<ul><li each="{menu}" ><a href="#{dest}" class="nav {title.toLowerCase()}">{title}</a></li></ul>', 'li { list-style-type: none; display: inline-block; padding: 10px; background-color: #eee; }', function(opts) {
     this.menu = [
       {
